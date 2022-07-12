886d92f26cb19544864d3fb4220dcd8c04ec7d39
Lucas <lucas.silva@tsaaviacao.com.br>
2022-07-12 09:06:53 -0300

    fix node-sass dependency

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,24 +1,24 @@
 <template>
 	<div>
 		<h1 class="title">All Products</h1>
-    	<p>{{length}} products</p>
-    	<table class="table is-striped">
-		    <thead>
-		      <tr>
-		        <th>Name</th>
-		        <th>Description</th>
-		        <th>Price</th>
-		        <th></th>
-		      </tr>
-		    </thead>
-		    <tbody>
-		      <tr v-for="product in products" track-by="id">
-		        <td>{{product.name}}</td>
-		        <td>{{product.description}}</td>
-		        <td>${{product.price}}</td>
-		        <td><button @click='addToCart(product)' class='button is-info'>Add to cart</button></td>
-		      </tr>
-		    </tbody>
+		<p>{{ length }} products</p>
+		<table class="table is-striped">
+			<thead>
+				<tr>
+					<th>Nome</th>
+					<th>Descrição</th>
+					<th>Prreço</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="product in products" track-by="id">
+					<td>{{ product.name }}</td>
+					<td>{{ product.description }}</td>
+					<td>${{ product.price }}</td>
+					<td><button @click='addToCart(product)' class='button is-info'>Add to cart</button></td>
+				</tr>
+			</tbody>
 		</table>
 	</div>
 </template>
@@ -27,17 +27,16 @@
 import { mapGetters, mapActions } from 'vuex'
 
 export default {
-  name: 'app',
-  computed: mapGetters({
-    products: 'allProducts',
-    length: 'getNumberOfProducts'
-  }),
-  methods: mapActions([
-    'addToCart'
-  ])
+	name: 'app',
+	computed: mapGetters({
+		products: 'allProducts',
+		length: 'getNumberOfProducts'
+	}),
+	methods: mapActions([
+		'addToCart'
+	])
 }
 </script>
 
 <style lang="scss">
-
 </style>
